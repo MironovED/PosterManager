82fe2181e7f02a6af5e3d65869053b357c1f6e7b
Evgeny Mironov <e.d.mironov@yandex.ru>
2022-02-13 21:06:58 +0300

    created tests

--- src/test/java/ru/netology/manager/PosterManagerTest.java
+++ src/test/java/ru/netology/manager/PosterManagerTest.java
@@ -1,5 +1,6 @@
 package ru.netology.manager;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -43,42 +44,6 @@ class PosterManagerTest {
     }
 
     @Test
-    void get5Films() {
-        Film first = new Film(1, "Бладшот");
-        Film second = new Film(2, "Вперед");
-        Film third = new Film(3, "Отель Белград");
-        Film fourth = new Film(4, "Джельтемены");
-        Film fifth = new Film(5, "Человек-невидимка");
-        Film sixth = new Film(6, "Тролли");
-        Film seventh = new Film(7, "Номер один");
-        Film eighth = new Film(8, "Назад в будущее");
-        Film ninth = new Film(9, "Командос");
-        Film ten = new Film(10, "Терминатор");
-        Film eleven = new Film(11, "Одноклассники");
-        Film twelfth = new Film(12, "Выживший");
-
-        PosterManager manager = new PosterManager(5);
-        manager.save(first);
-        manager.save(second);
-        manager.save(third);
-        manager.save(fourth);
-        manager.save(fifth);
-        manager.save(sixth);
-        manager.save(seventh);
-        manager.save(eighth);
-        manager.save(ninth);
-        manager.save(ten);
-        manager.save(eleven);
-        manager.save(twelfth);
-
-//        Film[] expected = {twelfth , eleven, ten, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
-        Film[] expected = {twelfth, eleven, ten, ninth, eighth};
-        Film[] actual = manager.getAll();
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
     void get9Films() {
         Film first = new Film(1, "Бладшот");
         Film second = new Film(2, "Вперед");
@@ -115,7 +80,7 @@ class PosterManagerTest {
     }
 
     @Test
-    void get11Films() {
+    void getFilmsWhenLimit11And12Films() {
         Film first = new Film(1, "Бладшот");
         Film second = new Film(2, "Вперед");
         Film third = new Film(3, "Отель Белград");
@@ -149,4 +114,137 @@ class PosterManagerTest {
 
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    void getFilmsWhenLimit10And12Films() {
+        Film first = new Film(1, "Бладшот");
+        Film second = new Film(2, "Вперед");
+        Film third = new Film(3, "Отель Белград");
+        Film fourth = new Film(4, "Джельтемены");
+        Film fifth = new Film(5, "Человек-невидимка");
+        Film sixth = new Film(6, "Тролли");
+        Film seventh = new Film(7, "Номер один");
+        Film eighth = new Film(8, "Назад в будущее");
+        Film ninth = new Film(9, "Командос");
+        Film ten = new Film(10, "Терминатор");
+        Film eleven = new Film(11, "Одноклассники");
+        Film twelfth = new Film(12, "Выживший");
+
+        PosterManager manager = new PosterManager();
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+        manager.save(fourth);
+        manager.save(fifth);
+        manager.save(sixth);
+        manager.save(seventh);
+        manager.save(eighth);
+        manager.save(ninth);
+        manager.save(ten);
+        manager.save(eleven);
+        manager.save(twelfth);
+
+
+        Film[] expected = {twelfth, eleven, ten, ninth, eighth, seventh, sixth, fifth, fourth, third };
+        Film[] actual = manager.getAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void get10FilmsWhenLimit10And10Films() {
+        Film first = new Film(1, "Бладшот");
+        Film second = new Film(2, "Вперед");
+        Film third = new Film(3, "Отель Белград");
+        Film fourth = new Film(4, "Джельтемены");
+        Film fifth = new Film(5, "Человек-невидимка");
+        Film sixth = new Film(6, "Тролли");
+        Film seventh = new Film(7, "Номер один");
+        Film eighth = new Film(8, "Назад в будущее");
+        Film ninth = new Film(9, "Командос");
+        Film ten = new Film(10, "Терминатор");
+
+
+        PosterManager manager = new PosterManager();
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+        manager.save(fourth);
+        manager.save(fifth);
+        manager.save(sixth);
+        manager.save(seventh);
+        manager.save(eighth);
+        manager.save(ninth);
+        manager.save(ten);
+
+
+        Film[] expected = { ten, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first };
+        Film[] actual = manager.getAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void getFilmsWhenLimit9And9Films() {
+        Film first = new Film(1, "Бладшот");
+        Film second = new Film(2, "Вперед");
+        Film third = new Film(3, "Отель Белград");
+        Film fourth = new Film(4, "Джельтемены");
+        Film fifth = new Film(5, "Человек-невидимка");
+        Film sixth = new Film(6, "Тролли");
+        Film seventh = new Film(7, "Номер один");
+        Film eighth = new Film(8, "Назад в будущее");
+        Film ninth = new Film(9, "Командос");
+
+
+        PosterManager manager = new PosterManager(9);
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+        manager.save(fourth);
+        manager.save(fifth);
+        manager.save(sixth);
+        manager.save(seventh);
+        manager.save(eighth);
+        manager.save(ninth);
+
+
+        Film[] expected = { ninth, eighth, seventh, sixth, fifth, fourth, third, second, first };
+        Film[] actual = manager.getAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    @Disabled
+    void getFilmsWhenLimit10And9Films() {
+        Film first = new Film(1, "Бладшот");
+        Film second = new Film(2, "Вперед");
+        Film third = new Film(3, "Отель Белград");
+        Film fourth = new Film(4, "Джельтемены");
+        Film fifth = new Film(5, "Человек-невидимка");
+        Film sixth = new Film(6, "Тролли");
+        Film seventh = new Film(7, "Номер один");
+        Film eighth = new Film(8, "Назад в будущее");
+        Film ninth = new Film(9, "Командос");
+
+
+        PosterManager manager = new PosterManager();
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+        manager.save(fourth);
+        manager.save(fifth);
+        manager.save(sixth);
+        manager.save(seventh);
+        manager.save(eighth);
+        manager.save(ninth);
+
+
+        Film[] expected = { ninth, eighth, seventh, sixth, fifth, fourth, third, second, first };
+        Film[] actual = manager.getAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
 }
